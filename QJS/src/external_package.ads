3b673ad33610f3328242ff714a1d745a1b2e676c
Piri Babayev <212310@usn.no>
2019-11-08 19:15:52 +0100

    Removed useless code

--- QJS/src/external_package.ads
+++
@@ -1,9 +0,0 @@
-package external_package is
-
-    function print_sensor (Deadline : Integer) return Integer;
-
-    function print_accelerometer (Deadline : Integer) return Integer;
-
-    function print_nav (Deadline : Integer) return Integer;
-
-end external_package;
